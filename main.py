5f1b333c3d1a852dcf0a811e3a1512dd237cb117
ArnavGuptas <91640989+ArnavGuptas@users.noreply.github.com>
2025-03-28 14:25:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,21 @@
 #Weighted Average Calculator
-numberofInput = input("Please enter number of digits to be proccesed: ")
+numberofInput = input("Please enter number of grades to be proccesed: ")
 numList = []
 for i in range(1, int(numberofInput)+1):
-    numInput = input("Enter your " + str(i) + " number: ")
+    numInput = input("Enter grade number " + str(i) + ": ")
     numList.append(numInput)
 
-
-
-def convertToGrades(numList):
-    gradesList = []
-    for each in numList:
-        if each < 60:
-            gradesList.append("F")
-        elif each < 70:
-            gradesList.append("D")
-        elif each < 80:
-            gradesList.append("C")
-        elif each < 90:
-            gradesList.append("B")
-        else:
-            gradesList.append("A")
-    return gradesList
-
-
-
-def findMeanMedianMode(numList):
-    meanMedianModeOutput = []
+def findMean(numList):
+    meanOutput = []
     mean = 0
     for each in numList:
         mean += int(each)
         mean = mean / len(numList)
-    meanMedianModeOutput.append("Mean: " + str(round(mean, 4)))
+    meanOutput = "Mean: " + str(round(mean, 4))
+    return meanOutput
+
+def findMedian(numList):
+    medianOutput=[]
     numList.sort()
     medianIndex = len(numList)-1
     medianIndex = medianIndex / 2
@@ -41,18 +26,27 @@ def findMeanMedianMode(numList):
         medianNum2 = int(numList[int(medianIndex - 0.5)])
         median = medianNum1 + medianNum2
         median = median / 2
-    meanMedianModeOutput.append("Median: " + str(median))
+    medianOutput = "Median: " + str(median)
+    return medianOutput
 
-    meanDictionary ={}
+def findMode(numList):
+    modeDictionary = {}
+    modeDictionaryValues= {}
+    modeOutput = {}
     for each in numList:
-        meanDictionary[each] = meanDictionary.get(each, 0) + 1
-    meanMedianModeOutput.append(meanDictionary)
-            
+        modeDictionary[each] = modeDictionary.get(each, 0) + 1
+        modeDictionaryValues = list(modeDictionary.values())
+    for i in range(0, len(modeDictionary)):
+        if modeDictionary[each] == modeDictionaryValues[0]:
+            return "No Mode Found"
+        else:    
+            modeOutput = "Mode: " + max(modeDictionary, key = modeDictionary.get)
+    return modeOutput
 
-
-    return meanMedianModeOutput
-
-
-
-
-print(findMeanMedianMode(numList))
+whichFunction = input("Would you like mean for a balanced average of all grades, median for an idea of the average grade without including outliers as much, or mode for the most average grade?: ")
+if whichFunction == "mean":
+    print(findMean(numList))
+elif whichFunction == "mode":
+    print(findMode(numList))
+elif whichFunction == "median":
+    print(findMedian(numList))
